0aeb44d06bb71ff72414867badb09f2391d88b14
kiran4399 <kiran4399@gmail.com>
2015-11-05 15:10:57 +0530

    pushed

--- eventManager-master/README.md
+++
@@ -1,4 +0,0 @@
-Event Management Portal
-==============
-
-(CS251 Assignment 4) 
--- eventManager-master/success.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-success">
-	
-<center>You are in! Click <a href='member.php' </a>here to enter member page</center>
-
-</div>
